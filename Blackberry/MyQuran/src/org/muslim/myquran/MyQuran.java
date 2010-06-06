e0b39abba8dfd1b3b49206d29d37ac400cf23332
hakimrie <hakimrie@53bf71e2-ba1d-11dd-8f2b-43a3dbd57a2c>
2010-06-06 04:35:44 +0000

    initial project structure

---
+++ Blackberry/MyQuran/src/org/muslim/myquran/MyQuran.java
@@ -0,0 +1,14 @@
+package org.muslim.myquran;
+
+import net.rim.device.api.ui.UiApplication;
+
+public class MyQuran extends UiApplication {
+	public static void main(String[] args){
+		MyQuran myquranInstance = new MyQuran();
+		myquranInstance.enterEventDispatcher();
+	}
+	
+	public MyQuran(){
+		
+	}
+}
---
+++ Blackberry/MyQuran/src/org/muslim/myquran/UI/MyQuranMainScreen.java
@@ -0,0 +1,9 @@
+package org.muslim.myquran.UI;
+
+import net.rim.device.api.ui.container.MainScreen;
+
+public class MyQuranMainScreen extends MainScreen {
+	public MyQuranMainScreen(){
+		super();
+	}
+}
